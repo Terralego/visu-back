50b5fb35ac4361bac047d4730eceffa1c231cd3f
Jeremie Pardou <jpardou@makina-corpus.com>
2019-08-21 16:51:48 +0200

    Add missing files

---
+++ local/terra-back-deploy/prod/init.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+# AS root
+set -ex
+SCRIPTSDIR="$(dirname $(readlink -f "$0"))"
+cd "$SCRIPTSDIR/.."
+TOPDIR=$(pwd)
+export APP_TYPE="${APP_TYPE:-docker}"
+export APP_USER="${APP_USER:-$APP_TYPE}"
+export APP_GROUP="$APP_USER"
+export USER_DIRS=". public/media"
+for i in $USER_DIRS;do
+    if [ ! -e "$i" ];then mkdir -p "$i";fi
+    chown $APP_USER:$APP_GROUP "$i"
+done
+if (find /etc/sudoers* -type f 2>/dev/null);then chown -Rf root:root /etc/sudoers*;fi
+exec gosu $APP_USER:$APP_GROUP bash -c "init/start.sh $@"
