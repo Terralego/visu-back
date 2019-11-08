6e760c5fba361bf7a4c7082d29da501c5d7b15e9
Nahuel ANGELINETTI <nahuel.angelinetti@makina-corpus.com>
2019-11-08 16:34:52 +0100

    Upgrade packages and configuration

--- src/project/urls.py
+++ src/project/urls.py
@@ -13,12 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import include, path
 
+from custom.receivers import *  # noqa
+
 urlpatterns = [
-    path('api/', include('geostore.urls')),
-    path('api/', include('terra_utils.urls')),
-    path('api/', include('terra_accounts.urls')),
-    path('api/geosource/', include('django_geosource.urls')),
-    path('api/geolayer/', include('terra_layer.urls')),
+    path('api/', include('terra_layer.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
