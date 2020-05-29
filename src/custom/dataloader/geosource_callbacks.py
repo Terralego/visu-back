93c23874e787315f5b74618c8121288930bed384
LePetitTim <timothee.de_montety@isen.yncrea.fr>
2020-05-29 13:41:03 +0200

    Fix signal and layer_callback

--- src/custom/dataloader/geosource_callbacks.py
+++ src/custom/dataloader/geosource_callbacks.py
@@ -3,8 +3,7 @@ import logging
 from django.contrib.auth.models import Group
 from django.contrib.gis.geos import GEOSGeometry
 from geostore.models import Layer, LayerGroup
-from terra_geocrud.models import CrudView, CrudViewProperty
-from terra_geocrud.properties.schema import sync_layer_schema
+
 from rest_framework.exceptions import MethodNotAllowed
 
 from custom.receivers import *  # noqa
@@ -13,45 +12,7 @@ logger = logging.getLogger(__name__)
 
 
 def layer_callback(geosource):
-
-    group_name = geosource.settings.pop('group', 'reference')
-
-    defaults = {
-        'settings': geosource.settings,
-    }
-
-    layer, _ = Layer.objects.get_or_create(name=geosource.slug, defaults=defaults, geom_type=geosource.geom_type)
-
-    layer_groups = Group.objects.filter(pk__in=geosource.settings.get('groups', []))
-
-    crud_view = CrudView.objects.get_or_create(layer=layer,
-                                               defaults={"name": layer.name, "order": 0})[0]
-
-    fields = geosource.fields.all()
-    for field in fields:
-        data_type = field.data_type
-        name = field.name
-        label = field.label
-        if data_type == 1:
-            final_type = "string"
-        elif data_type == 2:
-            final_type = "string"
-        elif data_type == 3:
-            final_type = "float"
-        elif data_type == 4:
-            final_type = "boolean"
-        property, created = CrudViewProperty.objects.get_or_create(view=crud_view, key=name)
-        property.json_schema = {"title": label, "type": final_type}
-        property.save()
-
-    sync_layer_schema(crud_view)
-    if set(layer.authorized_groups.all()) != set(layer_groups):
-        layer.authorized_groups.set(layer_groups)
-
-    if not layer.layer_groups.filter(name=group_name).exists():
-        group, _ = LayerGroup.objects.get_or_create(name=group_name)
-        group.layers.add(layer)
-
+    layer = Layer.objects.get(name=geosource.slug)
     return layer
 
 
--- src/custom/receivers.py
+++ src/custom/receivers.py
@@ -2,13 +2,15 @@ import logging
 import sys
 from io import BytesIO
 
+from django.contrib.auth.models import Group
 from django.core.management import call_command
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
-from django_geosource.models import Source
+from django_geosource.models import Source, GeoJSONSource
 from django_geosource.signals import refresh_data_done
-from geostore.models import Layer
-from terra_geocrud.models import CrudView
+from geostore.models import Layer, LayerGroup
+from terra_geocrud.models import CrudView, CrudViewProperty
+from terra_geocrud.properties.schema import sync_layer_schema
 
 logger = logging.getLogger(__name__)
 
@@ -26,8 +28,45 @@ def refresh_es(sender, **kwargs):
     logger.info('End of elasticsearch indexing')
 
 
-@receiver(post_save, sender=Layer)
-def create_CRUD_view_on_layer_creation(sender, **kwargs):
-    layer = kwargs['instance']
-    if Source.objects.filter(slug=layer.name).exists():
-        CrudView.objects.get_or_create(name=layer.name, layer=layer, order=0, )
+@receiver(post_save)
+def update_CRUD_view_on_source_creation(sender, **kwargs):
+    obj = kwargs['instance']
+    if isinstance(obj, Source):
+        source = obj
+        group_name = source.settings.pop('group', 'reference')
+
+        defaults = {
+            'settings': source.settings,
+        }
+
+        layer, created = Layer.objects.get_or_create(name=source.slug, defaults=defaults, geom_type=source.geom_type)
+
+        layer_groups = Group.objects.filter(pk__in=source.settings.get('groups', []))
+
+        if set(layer.authorized_groups.all()) != set(layer_groups):
+            layer.authorized_groups.set(layer_groups)
+
+        if not layer.layer_groups.filter(name=group_name).exists():
+            group, _ = LayerGroup.objects.get_or_create(name=group_name)
+            group.layers.add(layer)
+
+        crud_view = CrudView.objects.get_or_create(layer=layer,
+                                                   defaults={"name": layer.name, "order": 0})[0]
+        fields = source.fields.all()
+        for field in fields:
+            data_type = field.data_type
+            name = field.name
+            label = field.label
+            if data_type == 1:
+                final_type = "string"
+            elif data_type == 2:
+                final_type = "string"
+            elif data_type == 3:
+                final_type = "float"
+            elif data_type == 4:
+                final_type = "boolean"
+            property, created = CrudViewProperty.objects.get_or_create(view=crud_view, key=name)
+            property.json_schema = {"title": label, "type": final_type}
+            property.save()
+
+        sync_layer_schema(crud_view)
