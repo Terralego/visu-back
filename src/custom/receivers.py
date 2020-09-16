93902826729ea0d78bd663969ea095d2baff1694
Jeremie Pardou <571533+jrmi@users.noreply.github.com>
2020-09-16 12:33:51 +0200

    Add hack to void conflict between celery LoggingProxy and mondrian from bonobo

--- src/custom/receivers.py
+++ src/custom/receivers.py
@@ -12,10 +12,21 @@ logger = logging.getLogger(__name__)
 @receiver(refresh_data_done)
 def refresh_es(sender, **kwargs):
     try:
-        logger.info('Starting elasticsearch indexing')
-        sys.stdout.buffer = BytesIO()
+        logger.info('Starting elasticsearch indexing...')
+        try:
+            # Fix incompatibility with LoggingProxy from celery and 
+            # mondrian from bonobo
+            sys.stdout.buffer = BytesIO()
+            sys.stdout.encoding = None
+        except AttributeError:
+            # This is a hack, should fails with some conditions
+            pass
+
         call_command('etl_features_to_es', '-layer', kwargs['layer']) #noqa
-        logger.info('Elasticsearch indexing sucess')
-    except Exception:
-        logger.error('An error occurend during indexing', exc_info=True)
+
+        logger.info('Elasticsearch indexing succeed')
+
+    except:
+        logger.error('An error occurend during elastic search indexing', exc_info=True)
+
     logger.info('End of elasticsearch indexing')
