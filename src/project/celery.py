30388936af5425a02a77c329341aa5f39040bb15
Jeremie Pardou-Piquemal <571533+jrmi@users.noreply.github.com>
2021-09-14 12:55:30 +0200

    Change way periodic tasks are launched

--- src/project/celery.py
+++ src/project/celery.py
@@ -2,6 +2,20 @@ from celery import Celery
 
 app = Celery("terralego")
 
+
+@app.on_after_finalize.connect
+def setup_periodic_tasks(sender, **kwargs):
+    # Calls refresh check every 25 minutes
+    sender.add_periodic_task(60 * 25, run_auto_refresh_source.s())
+
+
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
 app.autodiscover_tasks()
+
+
+@app.task
+def run_auto_refresh_source():
+    from django_geosource.periodics import auto_refresh_source
+
+    auto_refresh_source()
